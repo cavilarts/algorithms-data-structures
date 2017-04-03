f860d043abfa5665bb6c4a36c94b8d6899c47c8a
cavilarts <cavilart21@gmail.com>
2017-04-02 21:06:19 -0400

    refactoring maximumpairwise file

--- maximunPairwiseProduct.js
+++ maximunPairwiseProduct.js
@@ -1,51 +1,48 @@
 var readline = require('readline'),
-    rl,
+    readLineInterface,
     size;
 
 process.stdin.setEncoding('utf8');
-rl = readline.createInterface({
-    input: process.stdin,
-    terminal: false
+readLineInterface = readline.createInterface({
+    "input": process.stdin,
+    "terminal": false
 });
 
-rl.on('line', (number) => {
-    var result;
+readLineInterface.on('line', (number) => {
+    var result,
+        parsedNumber = parseInt(number,10);
 
-    if (!size && parseInt(number, 10) > 1) {
-        size = parseInt(number, 10);
-    } else  {
-        result = MaxPairwiseProductFast(transformToArray(number));
+    if(!size && parsedNumber > 1) {
+        size = parsedNumber;
+    } else {
+        result = maxPairwiseProductFast(transformToArray(number));
         console.log(result);
         process.exit();
-    }
+    } 
 });
 
-function transformToArray(array) {
-    var splitArr = array.split(' '),
-        parsedActualValue,
-        elements = [];
+function transformToArray(string) {
+    var splitString = string.split(' ');
 
-    try {
-        for (var i = 0; i < size; i++) {
-            parsedActualValue = parseInt(splitArr[i], 10);
-
-            if (parsedActualValue !== NaN) {
-                elements.push(parsedActualValue);
-            }
-        }
-
-        return elements;
-    } catch (e) {
-        console.warn('cannot complete the action');
-    }
+    return validateArrayElements(splitString);
 }
 
-function MaxPairwiseProduct(array) {
+function validateArrayElements(array) {
+    return array.map(function(el) {
+        var parsedValue = parseInt(el, 10);
+
+        if (parsedValue) {
+            return parsedValue;
+        }
+    });
+}
+
+function maxPairwiseProduct(array) {
     var results = 0;
-    console.log(array);
-    for (var i = 0; i < array.length; ++i) {
-        for (var j = i + 1; j < array.length; ++j) {
-            if ((array[i] * array[j]) > results) {
+
+    for (var i = 0; i < array.length; i++) {
+        for (var j = i + 1; j < array.length; j++) {
+            if (array[i] * array[j] > results) {
                 results = array[i] * array[j];
             }
         }
@@ -54,56 +51,63 @@ function MaxPairwiseProduct(array) {
     return results;
 }
 
-function MaxPairwiseProductFast(array) {
-    var n = array.length,
-        max_index1 = -1,
-        max_index2 = -1;
+function maxPairwiseProductFast(array) {
+    var arrayLength = array.length,
+        firstMaxIndex = -1,
+        secondMaxIndex = -1;
 
-    for (var i = 0; i < n; ++i) {
-        if ((max_index1 === -1) || (array[i] >= array[max_index1])) {
-            max_index1 = i;
+    for (var i = 0; i < arrayLength; i++) {
+        if ((firstMaxIndex === -1) || (array[i] >= array[firstMaxIndex])) {
+            firstMaxIndex = i;
         }
     }
-    
 
-    for (var j = 0; j < n; ++j) {
-        if ((j !== max_index1) && (max_index2 === -1 || (array[j] >= array[max_index2]))){
-            max_index2 = j;
+    for (var j = 0; j < arrayLength; j++) {
+        if ((j !== firstMaxIndex) && (secondMaxIndex === -1 || (array[j] >= array[secondMaxIndex]))) {
+            secondMaxIndex = j;
         }
-
     }
 
-    return (array[max_index1] * array[max_index2]);
+    return array[firstMaxIndex] * array[secondMaxIndex];
 }
 
-function main() {
-    while ([1, 2].length) {
+function stressTests() {
+    while (true) {
         var a = "",
-            res1,
-            res2;
+            slow,
+            fast,
+            size = 0;
 
-        size = 0;
-        size = parseInt((Math.random() * 10) + 2, 10);
+        size = getSize();
 
-        for (var i = 0; i < size; ++i) {
-            if (i) {
-                a = a + ' ' + parseInt((Math.random() * 100000), 10);
-            } else {
-                a = parseInt((Math.random() * 100000), 10);
-            }
-        }
-        // console.log(a)
-        res1 = MaxPairwiseProduct(transformToArray(a));
-        res2 = MaxPairwiseProductFast(transformToArray(a));
+        for (var i = 0; i < size; i++) {
+           if (i) {
+               a = a + ' ' + getRandomNum(); 
+           } else {
+               a = getRandomNum();
+           }
+       }
 
-        if (res1 != res2) {
-          console.log("Wrong answer: " + res1 + ' ' + res2 + "\n") ;
-          break;
-        }
-        else {
+       console.log(a);
+       slow = maxPairwiseProduct(transformToArray(a));
+       fast = maxPairwiseProductFast(transformToArray(a));
+
+       if (slow !== fast) {
+           console.log("Wrong aswer" + slow + ' ' + fast + "\n");
+           break;
+       } else {
             console.log("OK\n");
-        }
+       }
+            
     }
 }
 
-// main();
+function getSize() {
+    return parseInt((Math.random() * 10) + 2, 10);
+}
+
+function getRandomNum() {
+    return parseInt((Math.random() * 100000), 10) + 1;
+}
+
+stressTests();
